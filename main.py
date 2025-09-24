1187992a3a1efcdab35719d938b383f4ab7344bc
Arttu Aarnio <arttueino@gmail.com>
2025-09-24 20:32:46 +0300

    added functionality so that running the main.py file reads the /books/frankenstein.txt file and prints it to the console

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        book_data = f.read()
+    return book_data
+
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    print(book)
+
+main()
+    
