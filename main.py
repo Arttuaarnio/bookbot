b76041172c45f48566ff2e936a49a3e9a85b25e9
Arttu Aarnio <arttueino@gmail.com>
2025-09-25 20:35:39 +0300

    added a new function to stats.py that counts how many time a character appears

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_book_words
+from stats import count_book_words, count_book_characters
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,7 +8,9 @@ def get_book_text(path):
 def main():
     book = get_book_text("./books/frankenstein.txt")
     word_count = count_book_words(book)
+    characters = count_book_characters(book)
     print(f"Found {word_count} total words")
+    print(characters)
 
 main()
     
--- stats.py
+++ stats.py
@@ -4,3 +4,19 @@ def count_book_words(book):
     for word in words:
         word_count += 1
     return word_count
+
+def count_book_characters(book):
+    characters = {}
+
+    for i in range(len(book)):
+        char = book[i]
+        char = char.lower()
+
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+
+    return characters
+
+
