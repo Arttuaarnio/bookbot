8e681262d1eb3aa4d72353117821cd95d3099f98
Arttu Aarnio <arttueino@gmail.com>
2025-09-27 21:23:36 +0300

    finished the program so you can run the program and add a book to it, no hard coded book path anymore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_book_words, count_book_characters, sort_characters
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,20 +7,26 @@ def get_book_text(path):
     return book_data
 
 def main():
-    book = get_book_text("./books/frankenstein.txt")
-    word_count = count_book_words(book)
-    characters = count_book_characters(book) 
-    sorted_characters = sort_characters(characters)
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        get_chosen_book = sys.argv[1]
+        book = get_book_text(get_chosen_book)
+        word_count = count_book_words(book)
+        characters = count_book_characters(book) 
+        sorted_characters = sort_characters(characters)
+        
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
     
-    for character in sorted_characters:
-        if character["char"].isalpha():
-            print(f"{character['char']}: {character['num']}")
+        for character in sorted_characters:
+            if character["char"].isalpha():
+                print(f"{character['char']}: {character['num']}")
 
 main()
     
